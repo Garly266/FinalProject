24a83100ec6a0a6b7e1cc1552da450d73cb61f18
Garly <tolya-lisichkin@yandex.ru>
2022-12-27 20:16:48 +0400

    Add task1

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] array = ReadInt();
+string[] result = Condition(array, 3);
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] Condition(string[] input, int n)
+{
+    string[] output = new string[Count(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int Count(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] ReadInt()
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
